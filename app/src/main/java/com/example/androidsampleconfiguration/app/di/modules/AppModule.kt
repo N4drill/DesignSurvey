b37cdcd9a69238d06ad8712d4ce3cd9e70f52b7e
Nadrill <pawel.gluszczak1@gmail.com>
2020-04-20 17:00:22 +0200

    adding appContext qualifier

--- app/src/main/java/com/example/androidsampleconfiguration/app/di/modules/AppModule.kt
+++ app/src/main/java/com/example/androidsampleconfiguration/app/di/modules/AppModule.kt
@@ -2,14 +2,14 @@ package com.example.androidsampleconfiguration.app.di.modules
 
 import android.content.Context
 import com.example.androidsampleconfiguration.app.App
+import com.example.androidsampleconfiguration.app.di.AppContext
 import dagger.Module
 import dagger.Provides
-import javax.inject.Singleton
 
 @Module
 class AppModule {
 
     @Provides
-    @Singleton
+    @AppContext
     fun provideApplicationContext(app: App): Context = app
 }
