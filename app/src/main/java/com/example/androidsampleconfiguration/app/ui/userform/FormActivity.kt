e1379995c456edd1d21ad5c74cd1e2c396c708e5
Pawel Gluszczak <gluszczakp@ryanair.com>
2020-05-04 17:48:21 +0200

    Form layout update, preparing all the data and logic associated with components. Prepared to send info to database

--- app/src/main/java/com/example/androidsampleconfiguration/app/ui/userform/FormActivity.kt
+++ app/src/main/java/com/example/androidsampleconfiguration/app/ui/userform/FormActivity.kt
@@ -1,26 +1,133 @@
 package com.example.androidsampleconfiguration.app.ui.userform
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
+import android.widget.SeekBar
 import androidx.databinding.DataBindingUtil
 import com.example.androidsampleconfiguration.R
+import com.example.androidsampleconfiguration.app.ui.userform.UserModel.Gender
+import com.example.androidsampleconfiguration.app.ui.userform.UserModel.Gender.FEMALE
+import com.example.androidsampleconfiguration.app.ui.userform.UserModel.Gender.MALE
+import com.example.androidsampleconfiguration.commons.SeekBarChangeListener
 import com.example.androidsampleconfiguration.databinding.ActivityFormBinding
 import dagger.android.support.DaggerAppCompatActivity
+import io.reactivex.disposables.CompositeDisposable
 import timber.log.Timber
+import kotlin.properties.Delegates
 
 class FormActivity : DaggerAppCompatActivity() {
+
+    private lateinit var binding: ActivityFormBinding
+
+    private val onDestroyDisposable = CompositeDisposable()
+    private var currentUserModel: UserModel by Delegates.observable(
+        UserModel(
+            gender = DEFAULT_GENDER,
+            age = DEFAULT_AGE,
+            designExperience = DEFAULT_EXP,
+            profession = DEFAULT_PROFESSION
+        )
+    ) { _, _, new ->
+        new.printInfo("New UserModel")
+        binding.changeGenderAppearance()
+        binding.tvCurrentAge.text = new.age.toString()
+        binding.tvCurrentExp.text = new.designExperience.formatDesignExperience()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         DataBindingUtil.setContentView<ActivityFormBinding>(this, R.layout.activity_form).apply {
-            setupLayout()
+            binding = this
+            currentUserModel = currentUserModel // TODO: Kinda tricky....
+            minAge = MIN_AGE
+            minExp = MIN_EXP
+            setupGenderSwitch()
+            setupProfessionSpinner()
+            setupAgeSeekBar()
+            setupExpSeekBar()
+            setupCta()
         }
 
         Timber.d("Form layout started")
     }
 
-    private fun ActivityFormBinding.setupLayout() {
-        setupProfessionSpinner()
+    private fun ActivityFormBinding.setupCta() {
+        btnAccept.setOnClickListener {
+            with(currentUserModel) {
+                //TODO: Setup model verification
+                //TODO: Format profession
+                //TODO: Save user model to firestore.
+
+                printInfo("Saving  UserMode")
+            }
+        }
+    }
+
+    private fun ActivityFormBinding.setupGenderSwitch() {
+        sGender.setOnCheckedChangeListener { _, isChecked ->
+            updateUserModel(
+                gender = when (isChecked) {
+                    true -> FEMALE
+                    false -> MALE
+                }
+            )
+        }
+
+    }
+
+    private fun updateUserModel(
+        gender: Gender = currentUserModel.gender,
+        age: Int = currentUserModel.age,
+        designExperience: Int = currentUserModel.designExperience,
+        profession: String = currentUserModel.profession
+    ) {
+        currentUserModel = currentUserModel.copy(
+            gender = gender,
+            age = age,
+            designExperience = designExperience,
+            profession = profession
+        )
+    }
+
+    private fun ActivityFormBinding.setupAgeSeekBar() {
+        with(seekAge) {
+            incrementProgressBy(AGE_STEP)
+            progress = DEFAULT_AGE
+            max = MAX_AGE
+            setOnSeekBarChangeListener(object : SeekBarChangeListener() {
+                override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                    val result = when {
+                        progress < MIN_AGE -> MIN_AGE
+                        progress > MAX_AGE -> MAX_AGE
+                        else -> progress
+                    }
+
+                    updateUserModel(age = result)
+                }
+            })
+        }
+    }
+
+    private fun ActivityFormBinding.setupExpSeekBar() {
+        with(seekExp) {
+            incrementProgressBy(EXP_STEP)
+            progress = DEFAULT_EXP
+            max = MAX_EXP
+            setOnSeekBarChangeListener(object : SeekBarChangeListener() {
+                override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                    val result = when {
+                        progress < MIN_EXP -> MIN_EXP
+                        progress > MAX_EXP -> MAX_EXP
+                        else -> progress
+                    }
+                    updateUserModel(designExperience = result)
+                }
+            })
+        }
     }
 
     private fun ActivityFormBinding.setupProfessionSpinner() {
@@ -30,6 +137,77 @@ class FormActivity : DaggerAppCompatActivity() {
             setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         }
         spinnerProfession.adapter = adapter
+        spinnerProfession.onItemSelectedListener = object : OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(spinner: AdapterView<*>, view: View?, position: Int, id: Long) {
+                updateUserModel(profession = spinner.getItemAtPosition(position).toString())
+            }
+
+        }
+
+        // TODO: SETUP ON CLICK UPDATE MODEL
     }
 
+    private fun ActivityFormBinding.changeGenderAppearance() {
+        when (currentUserModel.gender) {
+            MALE -> {
+                maleLabel.setTypeface(null, Typeface.BOLD)
+                femaleLabel.setTypeface(null, Typeface.NORMAL)
+            }
+            FEMALE -> {
+                maleLabel.setTypeface(null, Typeface.NORMAL)
+                femaleLabel.setTypeface(null, Typeface.BOLD)
+            }
+        }
+    }
+
+    private fun Int.formatDesignExperience(): String = getString(
+        when (this) {
+            1 -> R.string.less_than_year
+            2 -> R.string.one_two_years
+            3 -> R.string.two_tree_years
+            4 -> R.string.more_than_three
+            else -> R.string.no_experience
+        }
+    )
+
+    override fun onDestroy() {
+        super.onDestroy()
+        onDestroyDisposable.clear()
+    }
+
+    companion object {
+        private const val MIN_AGE = 12
+        private const val MAX_AGE = 80
+        private const val AGE_STEP = 1
+        private const val MIN_EXP = 0
+        private const val MAX_EXP = 4
+        private const val EXP_STEP = 1
+        //DEFAULTS
+        private val DEFAULT_GENDER = MALE
+        private const val DEFAULT_AGE = 20
+        private const val DEFAULT_PROFESSION = "Non Programmer" // TODO: Update to get this from resources
+        private const val DEFAULT_EXP = 0
+
+    }
+}
+
+private fun UserModel.printInfo(message: String = " UserModel update -> ") {
+    Timber.d("$message -> Gender: $gender, Age: $age, Experience: $designExperience, Profession: $profession")
+}
+
+data class UserModel(
+    val gender: Gender,
+    val age: Int,
+    val profession: String,
+    val designExperience: Int
+) {
+
+    enum class Gender {
+        MALE,
+        FEMALE
+    }
 }
---
+++ app/src/main/java/com/example/androidsampleconfiguration/commons/SeekBarChangeListener.kt
@@ -0,0 +1,14 @@
+package com.example.androidsampleconfiguration.commons
+
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+
+abstract class SeekBarChangeListener : OnSeekBarChangeListener {
+    override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+    }
+
+    override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+    }
+}
