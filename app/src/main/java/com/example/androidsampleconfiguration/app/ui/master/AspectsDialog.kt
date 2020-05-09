d230697ecd587d324dc3b4b6e625fc6e054745bb
Pawel Gluszczak <gluszczakp@ryanair.com>
2020-05-10 00:34:09 +0200

    handling clicking of aspects

--- app/src/main/java/com/example/androidsampleconfiguration/app/ui/master/AspectsDialog.kt
+++ app/src/main/java/com/example/androidsampleconfiguration/app/ui/master/AspectsDialog.kt
@@ -1,18 +1,34 @@
 package com.example.androidsampleconfiguration.app.ui.master
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.androidsampleconfiguration.app.ui.master.AspectAdapter.Aspect
+import com.example.androidsampleconfiguration.app.ui.master.AspectAdapter.AspectListener
+import com.example.androidsampleconfiguration.commons.extensions.autoNotify
 import com.example.androidsampleconfiguration.databinding.DialogAspectsBinding
 import com.example.androidsampleconfiguration.databinding.ItemAspectBinding
 import dagger.android.support.DaggerDialogFragment
+import timber.log.Timber
+import kotlin.properties.Delegates
 
-class AspectsDialog : DaggerDialogFragment() {
+class AspectsDialog : DaggerDialogFragment(), AspectListener {
+    lateinit var aspectAdapter: AspectAdapter
+
+    override fun aspectClicked(aspect: Aspect) {
+        Timber.d("Aspect ${aspect.title} clicked")
+        aspectAdapter.items = aspectAdapter.items.map {
+            if (aspect.title == it.title) {
+                it.copy(selected = !it.selected)
+            } else {
+                it
+            }
+        }
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
         DialogAspectsBinding.inflate(inflater, container, false).apply {
             setupRecycler(listOf("COLOR", "OTHER1", "OTHER3"))
@@ -21,34 +37,50 @@ class AspectsDialog : DaggerDialogFragment() {
     private fun DialogAspectsBinding.setupRecycler(aspects: List<String>) {
         rvAspectsRecycler.apply {
             layoutManager = LinearLayoutManager(context)
-            adapter = AspectAdapter(aspects)
-            addItemDecoration(createItemDecorator(context))
+            adapter = AspectAdapter(aspectListener = this@AspectsDialog).also { aspectAdapter = it }
         }
+        aspectAdapter.items = aspects.toAspects()
     }
 }
 
-fun createItemDecorator(context: Context): DividerItemDecoration {
-    return DividerItemDecoration(context, RecyclerView.HORIZONTAL)
-}
+fun List<String>.toAspects(): List<Aspect> = map { Aspect(title = it, selected = false) }
 
-class AspectAdapter(val aspects: List<String>) : RecyclerView.Adapter<AspectAdapter.AspectViewHolder>() {
+class AspectAdapter(private val aspectListener: AspectListener) :
+    RecyclerView.Adapter<AspectAdapter.AspectViewHolder>() {
+
+    var items: List<Aspect> by Delegates.observable(emptyList()) { _, old, new ->
+        autoNotify(old, new) { other -> other.title == title && other.selected == selected }
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AspectViewHolder = AspectViewHolder(
-        ItemAspectBinding.inflate(
+        binding = ItemAspectBinding.inflate(
             LayoutInflater.from(parent.context),
             parent, false
-        )
+        ),
+        aspectListener = aspectListener
     )
 
-    override fun getItemCount(): Int = aspects.size
+    override fun getItemCount(): Int = items.size
 
     override fun onBindViewHolder(holder: AspectViewHolder, position: Int) {
-        holder.bind(aspects[position])
+        holder.bind(items[position])
     }
 
-    class AspectViewHolder(val binding: ItemAspectBinding) : RecyclerView.ViewHolder(binding.root) {
+    class AspectViewHolder(val binding: ItemAspectBinding, private val aspectListener: AspectListener) :
+        RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(aspect: String) {
-            binding.tvAspectName.text = aspect
+        fun bind(aspect: Aspect) {
+            binding.tvAspectName.text = aspect.title
+            binding.selected = aspect.selected
+            binding.tvAspectName.setOnClickListener {
+                aspectListener.aspectClicked(aspect = aspect)
+            }
         }
+    }
+
+    data class Aspect(val title: String, val selected: Boolean)
+
+    interface AspectListener {
+        fun aspectClicked(aspect: Aspect)
     }
 }
